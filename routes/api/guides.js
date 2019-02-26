54e9024fbcd63a7fc1118576964fdc689301cd6f
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2019-02-26 16:10:18 +0900

    Registration route and functionality added

--- routes/api/guides.js
+++ routes/api/guides.js
@@ -9,4 +9,27 @@ const Guide = require("../../models/Guide");
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Guides works" }));
 
+// @route  GET api/users/register
+// @desc   Register user
+// @access Public
+router.post("/register", (req, res) => {
+  Guide.findOne({ email: req.body.email }).then(guide => {
+    if (guide) {
+      return res.status(400).json({ email: "Email exists" });
+    } else {
+      //   Create new User
+      const newGuide = new Guide({
+        name: req.body.name,
+        email: req.body.email,
+        location: req.body.location,
+        password: req.body.password
+      });
+      newGuide
+        .save()
+        .then(guide => res.json(guide))
+        .catch(err => console.log(err));
+    }
+  });
+});
+
 module.exports = router;
