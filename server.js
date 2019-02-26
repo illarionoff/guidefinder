33c29e4625455f35c044617c2c8f6672877547b3
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2019-02-26 15:48:03 +0900

    Server added

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Body parse middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Mongoose connection
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
